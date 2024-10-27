5f4251de5f85970934144aca427aca8118676e51
tushar-sadat <tusharsadat30@gmail.com>
2024-10-27 22:57:07 +0600

    add multiple columns in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
