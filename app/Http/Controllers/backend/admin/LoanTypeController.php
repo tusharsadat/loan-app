de9ff1c5232a9f50cc2023c1012d3c1b2992f705
tushar-sadat <tusharsadat30@gmail.com>
2024-11-09 01:24:46 +0600

    create LoanTypeController and route

---
+++ app/Http/Controllers/backend/admin/LoanTypeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\backend\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoanTypeController extends Controller
+{
+    //
+}
--- resources/views/admin/sections/sideber.blade.php
+++ resources/views/admin/sections/sideber.blade.php
@@ -9,7 +9,7 @@
         <li class="my-3">
             <div x-data="{ open: false }">
                 <button @click="open = !open" class="flex items-center text-gray-300 hover:text-white">
-                    Users
+                    User Management
                     <svg x-show="!open" class="ml-auto h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
@@ -28,7 +28,7 @@
         <li class="my-3">
             <div x-data="{ open: false }">
                 <button @click="open = !open" class="flex items-center text-gray-300 hover:text-white">
-                    Products
+                    Loan Type Management
                     <svg x-show="!open" class="ml-auto h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
@@ -39,7 +39,8 @@
                     </svg>
                 </button>
                 <ul x-show="open" class="ml-4 mt-2 space-y-2">
-                    <li><a href="#" class="text-gray-300 hover:text-white">All Products</a></li>
+                    <li><a href="{{ route('admin.allLoanType') }}" class="text-gray-300 hover:text-white">View Lone
+                            Type</a></li>
                     <li><a href="#" class="text-gray-300 hover:text-white">Add Product</a></li>
                 </ul>
             </div>
